3630bb9985a738b9583bfa65f9ab2405d6ce0c8f
svicervlad <vlad.sadretdinov@jet.dev>
2022-08-26 16:22:08 +0300

    fix: env

--- up_individuals.sh
+++ up_individuals.sh
@@ -5,7 +5,7 @@ PROD_HOST="whoami-prod"
 
 docker run -d \
     -l "traefik.http.routers.$DEV.rule=Host(\`$DEV_HOST.docker.localhost\`)" \
-    --net traefic_default \
+    --net traefik_default \
     --net-alias $DEV \
     --name $DEV \
     --hostname $DEV \
@@ -14,7 +14,7 @@ docker run -d \
 
 docker run -d \
     -l "traefik.http.routers.$PROD.rule=Host(\`$PROD_HOST.docker.localhost\`)" \
-    --net traefic_default \
+    --net traefik_default \
     --net-alias $PROD \
     --name $PROD \
     --hostname $PROD \
